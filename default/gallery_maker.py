efe99247d59cbf63455eab07484bfc6417339f16
Richard Orilla <orillarichard@gmail.com>
2021-04-16 18:01:36 +0800

    added gallery UI base

---
+++ default/gallery_maker.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+def scan(folder, mode=1):
+    results = []
+    for i in os.scandir(folder):
+        if mode == 1:
+            if i.is_file():
+                results.append(i)
+        if mode == 2:
+            if i.is_dir():
+                results.append(i)
+    return results
+
+if __name__ == "__main__":
+    result = {}
+    folders = scan("gallery", 2)
+    for folder in folders:
+        result[folder.name] = {
+            "path": folder.path,
+            "files": []
+        }
+        files = scan(folder.path)
+        for file in files:
+            result[folder.name]["files"].append({
+                "name": file.name,
+                "path": file.path
+            })
+    f = open("gallery.js", "w")
+    f.write("var pictures = " + json.dumps(result))
+    f.close()
+    
