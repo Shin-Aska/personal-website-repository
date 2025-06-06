cc83854ab902a5f658c214bebdbbe28039b2ea9e
Richard Louie Orilla (GITHUB-KEY-MAIN-PC) <Orillarichard@pm.me>
2025-06-06 14:35:21 +0800

    Fix language handling for Mermaid code blocks in publisher

--- publishers/default.py
+++ publishers/default.py
@@ -4,8 +4,11 @@ from publishers.base import Publisher
 
 class DefaultThemePublisher(Publisher):
     def _generate_codeblock_content(self, html_content: str, line: str, element: MarkdownElement) -> str:
+        language: str = element.extra.get('language', 'text')
+        if language == 'mermaid':
+            language = 'text'
         html_content = self._push_to_html_content(html_content, '<pre>')
-        html_content = self._push_to_html_content(html_content, f'<code class="language-{element.extra["language"]}">', 1)
+        html_content = self._push_to_html_content(html_content, f'<code class="language-{language}">', 1)
         for line in element.content:
             html_content = self._push_to_html_content(html_content, line, Publisher.base_padding * -1, add_new_line_per_join=False)
         html_content = self._push_to_html_content(html_content, '</code>', 1)
